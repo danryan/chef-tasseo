94bd1a5c471275664065bab9d36e19549e150db1
Joseph Anthony Pasquale Holsten <joseph@josephholsten.com>
2012-11-24 06:28:30 +0000

    FC001: Use strings in preference to symbols to access node attributes

--- recipes/default.rb
+++ recipes/default.rb
@@ -19,42 +19,42 @@
 
 graphite_node = search(:node, 'recipes:graphite\:\:default').first
 
-if node[:fqdn] != graphite_node[:fqdn]
-  graphite_url = "https://#{graphite_node[:fqdn]}"
-  node.set[:tasseo][:graphite_url] = graphite_url
+if node['fqdn'] != graphite_node['fqdn']
+  graphite_url = "https://#{graphite_node['fqdn']}"
+  node.set['tasseo']['graphite_url'] = graphite_url
 end
 
-include_recipe "runit"
+include_recipe 'runit'
 
-git node[:tasseo][:home_dir] do
-  repository node[:tasseo][:git_repo]
-  reference "master"
+git node['tasseo']['home_dir'] do
+  repository node['tasseo']['git_repo']
+  reference 'master'
   action :checkout
-  notifies :restart, "service[tasseo]", :delayed
+  notifies :restart, 'service[tasseo]', :delayed
 end
 
-user node[:tasseo][:user] do
-  comment "Tasseo User"
+user node['tasseo']['user'] do
+  comment 'Tasseo User'
   system true
-  shell "/bin/false"
+  shell '/bin/false'
 end
 
-directory node[:tasseo][:log_dir] do
+directory node['tasseo']['log_dir'] do
   mode 0755
-  owner node[:tasseo][:user]
-  group node[:tasseo][:user]
+  owner node['tasseo']['user']
+  group node['tasseo']['user']
 end
 
-package "ruby-dev"
-package "build-essential"
+package 'ruby-dev'
+package 'build-essential'
 
-gem_package "bundler"
+gem_package 'bundler'
 
-execute "bundle install" do
-  command "bundle install"
-  cwd node[:tasseo][:home_dir]
+execute 'bundle install' do
+  command 'bundle install'
+  cwd node['tasseo']['home_dir']
 end
 
-runit_service "tasseo" do
-  options node[:tasseo]
+runit_service 'tasseo' do
+  options node['tasseo']
 end
